73f7ced4dab57c61edb9775b559622d4b3b81516
Sarah Wang <wangdongli0102@163.com>
2018-04-06 16:37:34 +0800

    [date]

--- data_access/craw_data_run.py
+++ data_access/craw_data_run.py
@@ -12,7 +12,7 @@ from back_test.bkt_option_set import BktOptionSet
 
 w.start()
 
-date = datetime.date(2018, 4, 2)
+date = datetime.date(2018, 4, 4)
 dt_date = date.strftime("%Y-%m-%d")
 print(dt_date)
 
