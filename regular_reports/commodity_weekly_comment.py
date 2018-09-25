0cb29119b29b7f881140f6d8503f782f55ee276d
Sarah Wang <wangdongli0102@163.com>
2018-09-25 14:19:14 +0800

    [run weekly report]

--- regular_reports/commodity_weekly_comment.py
+++ regular_reports/commodity_weekly_comment.py
@@ -187,8 +187,8 @@ def fun_report_namecode(namecode):
 
 
 # Eval Settings and Data
-dt_date = datetime.date(2018, 9, 14)  # Set as Friday
-dt_yesterday = datetime.date(2018, 9, 7) # last friday
+dt_date = datetime.date(2018, 9, 21)  # Set as Friday
+dt_yesterday = datetime.date(2018, 9, 14) # last friday
 
 # Eval Settings and Data
 
